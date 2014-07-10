32f3b7abc896a98678e74130777df2d67050a6c8
Seth Fitzsimmons <seth@mojodna.net>
2014-07-09 19:39:49 -0700

    Initial version

---
+++ README.md
@@ -0,0 +1,6 @@
+# kevlar
+
+Synthetic load generation.
+
+This assumes a source (`SOURCE_URL`) that outputs syslog lines (from Fastly, in
+this case) and a target (`TARGET_URL`) to send matching requests to.
---
+++ index.js
@@ -0,0 +1,55 @@
+"use strict";
+
+var http = require("http");
+
+var _ = require("highland"),
+    env = require("require-env"),
+    request = require("request");
+
+var BinarySplitter = require("./lib/binary-splitter");
+
+http.globalAgent.maxSockets = Infinity;
+
+var SYSLOG_RE = /^(<[0-9]+>)([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z) ([^ ]+) ([^[]+)\[([^\]]+)\]: (.*)$/;
+
+_(request.get(env.require("SOURCE_URL")).pipe(new BinarySplitter()))
+  .invoke("toString")
+  .invoke("trim")
+  .map(function(line) {
+    var parts;
+    
+    if ((parts = line.match(SYSLOG_RE))) {
+      var req = parts[6],
+          path = req.replace(/\"/g, "").split(" ", 2).pop();
+
+      return path;
+    }
+
+    console.warn("Invalid line:", line);
+
+    return "";
+  })
+  .filter(function(path) {
+    return path.match(/\/toner\//);
+  })
+  .map(function(path) {
+    return path.replace(/\/toner/, "");
+  })
+  .map(function(path) {
+    request.head(env.require("TARGET_URL") + path, function(err, rsp, body) {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+
+      if (rsp.statusCode === 200) {
+        // console.log("%s: %s", path, rsp.headers["x-response-time"], rsp.headers["x-cache"], rsp.headers["x-cache-hits"]);
+        return;
+      }
+
+      console.log("%s: %d", path, rsp.statusCode);
+    });
+
+    return "";
+  })
+  .pipe(process.stdout);
